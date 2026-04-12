f5ce12dbaaeb392b129b9cea5480364c5356f05e
Radiolaba <radiolaba@proton.me>
2026-04-12 09:53:04 +0300

    Добавлены комментарии к файлу DS1054Z_VS26.cpp

--- DS1054Z_VS26/DS1054Z_VS26.cpp
+++ DS1054Z_VS26/DS1054Z_VS26.cpp
@@ -1,13 +1,16 @@
-﻿#include "Oscill.h"
-#include <iostream>
+﻿#include "Oscill.h" // подключение заголовочного файла, где объявлен класс OscilloscopeRigol_DS1054Z и его методы
+#include <iostream> // подлкючение стандартной библиотеки C++ для ввода/вывода через консоль
 
-int main()
+int main()			// главная функция
 {
-	OscilloscopeRigol_DS1054Z oscill;
-	oscill.connect();
-	Sleep(600);
-	oscill.setup();
-	Sleep(600);
-	auto res = oscill.getRaw16BitSignal(1000, 20000);
+	OscilloscopeRigol_DS1054Z oscill; // создание объекта oscill класса OscilloscopeRigol_DS1054Z для взаимодействия с осциллографом
+	oscill.connect(); // вызов метода подключения к осциллографу
+	Sleep(600);			// пауза 600 мс
+	oscill.setup();		// вызов метода настройки осциллографа
+	Sleep(600);			// пауза 600 мс
+	auto res = oscill.getRaw16BitSignal(1000, 20000);	// вызов метода чтения данных
+	//запрос сырых данных из памяти осциллографа и возвращение vector<uint16_t>
 	return 0;
 }
+// необходимо добавить преобразование vector<uint16_t> в пары (time, voltage) и запись их в файл
+// необходимо добавить oscill.disconnect() в конец
