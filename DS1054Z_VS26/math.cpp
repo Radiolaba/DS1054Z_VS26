9278dc198f7b021a182d8c2afc6e5d70b051fe9d
Radiolaba <radiolaba@proton.me>
2026-04-16 12:26:46 +0300

    Добавлены комментарии

--- DS1054Z_VS26/math.cpp
+++ DS1054Z_VS26/math.cpp
@@ -1,16 +1,19 @@
-#include <vector>
-#include <cstdint>
-#include <algorithm>
-#include <iterator>
+#include <vector> // подключение типа динамического массива (вектора) std::vector
+#include <cstdint> // подключение типов фиксированной ширины, в т.ч. uint16_t
+#include <algorithm> // подключение стандартных алгоритмов, в т.ч. std::transform
+#include <iterator> // подключение итераторов, в т.ч. std::back_inserter
 
-std::vector<double> to_double_vector(const std::vector<uint16_t>& input)
+// объявление функции преобразования вектора std::vector<uint16_t> в вектор std::vector<double> 
+std::vector<double> to_double_vector(const std::vector<uint16_t>& input) 
 {
-    std::vector<double> output;
-    output.reserve(input.size());
+    std::vector<double> output; // создание пустовго вектора
+    output.reserve(input.size()); // выделяется память под количество элементов, равное размеру input
 
-    std::transform(input.begin(), input.end(),
-        std::back_inserter(output),
-        [](uint16_t x) { return static_cast<double>(x); });
+    std::transform(input.begin(), input.end(), std::back_inserter(output), [](uint16_t x)
+        {
+            return static_cast<double>(x);
+        }
+    );
 
     return output;
 }
--- DS1054Z_VS26/math.h
+++ DS1054Z_VS26/math.h
@@ -1,4 +1,4 @@
-#include <vector>
-#pragma once
+#include <vector> // подключение типа динамического массива (вектора) std::vector
+#pragma once // защита от повторного включения math.h
 
-std::vector<double> to_double_vector(const std::vector<uint16_t>& input);
+std::vector<double> to_double_vector(const std::vector<uint16_t>& input); // объявление функции
